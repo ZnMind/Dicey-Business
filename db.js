3d9e56d0460641f52ccd7038c8c0ae5b10146c63
Dan Mann <damann1072@gmail.com>
2022-03-04 23:44:07 -0600

    Added some things plus made it look cooler

--- db.js
+++ db.js
@@ -12,9 +12,19 @@ class Die {
     create() {
         let div = document.createElement("div");
         div.className = "newDie"
+        if (totalDie == 0) {
+            let sumButt = document.createElement("button");
+            sumButt.appendChild(document.createTextNode("Sum Dice"));
+            document.getElementById("buttonDie").appendChild(sumButt);
+            sumButt.addEventListener("click", function () { new Die().sumDice() });
+        }
         div.id = totalDie + 1;
+        div.addEventListener("click", function() {
+            div.removeChild(div.childNodes[0])
+            div.appendChild(document.createTextNode(new Die().roll().value));
+        })
         this.roll();
-        let text = document.createTextNode(Die.value)
+        let text = document.createTextNode(Die.value);
         div.appendChild(text);
         document.body.appendChild(div);
         totalDie++;
@@ -36,15 +46,23 @@ class Die {
             document.body.appendChild(newDiv);
         }
     }
+
+    sumDice() {
+        let sum = 0;
+        for (let i = 0; i < dieArr.length; i++) {
+            sum += dieArr[i].value;
+        }
+        if (totalDie == 1) {
+            alert(`There is ${totalDie} die on the page with a face value of ${sum}`);
+        } else {
+            alert(`There are ${totalDie} dice on the page. The sum of these dice are ${sum} pips averaging out to ${Math.floor((sum / totalDie) * 100) / 100} pips per die.`);
+        }
+    }
 }
 
 let totalDie = 0;
 let dieArr = [];
 
-document.getElementById("genDie").addEventListener("click", function() {
-    new Die().create();
-});
+document.getElementById("genDie").addEventListener("click", function() { new Die().create() });
 
-document.getElementById("rollDie").addEventListener("click", function() {
-    new Die().reroll();
-});
+document.getElementById("rollDie").addEventListener("click", function() { new Die().reroll() });
