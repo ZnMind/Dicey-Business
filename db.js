ee1924b86abfdf4716f602dca51d4a2435afce1b
Dan Mann <damann1072@gmail.com>
2022-03-09 10:52:43 -0600

    What I have is working, but I keep breaking it adding new features. Pushing now before I break it with more features

--- db.js
+++ db.js
@@ -1,8 +1,10 @@
 class Die {
     value;
+    number;
 
     constructor() {
         this.value = Die.value;
+        this.number = Die.number;
     }
 
     get value() {
@@ -14,21 +16,33 @@ class Die {
         div.className = "newDie"
         if (totalDie == 0) {
             let sumButt = document.createElement("button");
+            let getFunk = document.createElement("button");
             sumButt.appendChild(document.createTextNode("Sum Dice"));
+            getFunk.appendChild(document.createTextNode("Get Funky"));
             document.getElementById("buttonDie").appendChild(sumButt);
+            document.getElementById("buttonDie").appendChild(getFunk);
             sumButt.addEventListener("click", function () { new Die().sumDice() });
+            getFunk.addEventListener("click", function () { new Die().funky() });
         }
         div.id = totalDie + 1;
-        div.addEventListener("click", function() {
+        div.addEventListener("click", function () {
+            dieArr[div.id - 1] = new Die().roll();
             div.removeChild(div.childNodes[0])
-            div.appendChild(document.createTextNode(new Die().roll().value));
+            div.appendChild(document.createTextNode(Die.value));
         })
-        this.roll();
+        /* div.addEventListener("dblclick", function () {
+            div.remove();
+            removedDie++;
+            let begArr = dieArr.slice(0, [this.id - 1]);
+            let endArr = dieArr.slice(this.id, dieArr.length);
+            dieArr = begArr.concat(endArr);
+            console.log(dieArr);
+        }) */
+        totalDie++;
+        dieArr.push(this.roll())
         let text = document.createTextNode(Die.value);
         div.appendChild(text);
         document.body.appendChild(div);
-        totalDie++;
-        dieArr.push(new Die());
     }
     
     roll() {
@@ -45,6 +59,7 @@ class Die {
             newDiv.appendChild(newText);
             document.body.appendChild(newDiv);
         }
+        //console.log(dieArr);
     }
 
     sumDice() {
@@ -55,14 +70,53 @@ class Die {
         if (totalDie == 1) {
             alert(`There is ${totalDie} die on the page with a face value of ${sum}`);
         } else {
-            alert(`There are ${totalDie} dice on the page. The sum of these dice are ${sum} pips averaging out to ${Math.floor((sum / totalDie) * 100) / 100} pips per die.`);
+            alert(`There are ${totalDie} dice on the page. The sum of pips on the dice are ${sum} averaging out to ${Math.floor((sum / totalDie) * 100) / 100} pips per die.`);
+        }
+    }
+
+    clear() {
+        Die.value = 0;
+        console.log(this.id);
+        return Die.value;
+    }
+
+    funky() {
+        for (let i = 0; i < dieArr.length; i++) {
+            switch (dieArr[i].value) {
+                case 1:
+                    document.getElementById(`${i + 1}`).removeChild(document.getElementById(`${i + 1}`).childNodes[0])
+                    document.getElementById(`${i + 1}`).innerHTML = "<font size='100'>&#9856</font>"
+                    console.log(document.getElementById(`${i + 1}`));
+                    break;
+                case 2:
+                    document.getElementById(`${i + 1}`).className = ("newDie funky");
+                    console.log(document.getElementById(`${i + 1}`));
+                    break;
+                case 3:
+                    document.getElementById(`${i + 1}`).className = ("newDie funky");
+                    console.log(document.getElementById(`${i + 1}`));
+                    break;
+                case 4:
+                    document.getElementById(`${i + 1}`).className = ("newDie funky");
+                    console.log(document.getElementById(`${i + 1}`));
+                    break;
+                case 5:
+                    //document.getElementById(`${i + 1}`).className = ("newDie funky");
+                    console.log(document.getElementById(`${i + 1}`));
+                    break;
+                case 6:
+                    //document.getElementById(`${i + 1}`).className = ("newDie funky");
+                    console.log(document.getElementById(`${i + 1}`));
+                    break;
+            }
         }
     }
 }
 
 let totalDie = 0;
+let removedDie = 0;
 let dieArr = [];
 
-document.getElementById("genDie").addEventListener("click", function() { new Die().create() });
+document.getElementById("genDie").addEventListener("click", function () { new Die().create() });
 
-document.getElementById("rollDie").addEventListener("click", function() { new Die().reroll() });
+document.getElementById("rollDie").addEventListener("click", function () { new Die().reroll() });
